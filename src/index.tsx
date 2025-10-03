8721bbbe443d56bf640bf00552143018c7eac11b
patch-demon <patch.demon115@gmail.com>
2025-10-03 15:09:56 +0900

    added kinde-auth

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { KindeProvider } from "@kinde-oss/kinde-auth-react";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -8,9 +9,16 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  <KindeProvider
+    clientId={process.env.VITE_KINDE_CLIENT_ID}
+    domain={process.env.VITE_KINDE_DOMAIN}
+    logoutUri={process.env.VITE_KINDE_LOGOUT_URL}
+    redirectUri={process.env.VITE_KINDE_REDIRECT_URL}
+    // When running local against a custom domain, include the line below
+    // useInsecureForRefreshToken={true}
+  >
     <App />
-  </React.StrictMode>
+  </KindeProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
